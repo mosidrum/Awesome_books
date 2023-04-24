bbc6a6fa82059b72c5bcae66f0a3ede43aa3e424
mosidrum <mosiokanga@gmail.com>
2023-04-24 15:05:55 +0100

    fix linters errors

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 if (localStorage.getItem('Added Books') == null) {
-  localStorage.setItem('Added Books', JSON.stringify([])); 
+  localStorage.setItem('Added Books', JSON.stringify([]));
 }
 
 const storeData = JSON.parse(localStorage.getItem('Added Books'));
@@ -7,37 +7,6 @@ const storeData = JSON.parse(localStorage.getItem('Added Books'));
 function updateData() {
   localStorage.setItem('Added Books', JSON.stringify(storeData));
 }
-
-const bookForm = document.querySelector('form')
-bookForm.addEventListener('submit', (e) => {
-
-  const title = document.querySelector('.title')
-  const author = document.querySelector('.author')
-  e.preventDefault();
-  addNewData(title.value, author.value);
-});
-
-function createBook(arr) {
-  let books = '';
-  for (let i = 0; i < arr.length; i++) {
-    books +=`
-      <p>${arr[i].title}</p>
-      <p>${arr[i].author}</p>
-      <button onclick = "removeBook(${i})">Remove</button>
-      <hr/>
-    `;
-  }
-  return books
-}
-
-function displayBooks() {
-  const listOfBooks = document.querySelector('.container');
-  listOfBooks.innerHTML = `
-    <ul>
-      ${createBook(storeData)}
-    </ul>
-  `
-};
 
 function addNewData(bookTitle, bookAuthor) {
   const Book = {
@@ -49,6 +18,36 @@ function addNewData(bookTitle, bookAuthor) {
   displayBooks();
 }
 
+const bookForm = document.querySelector('form');
+bookForm.addEventListener('submit', (e) => {
+  const title = document.querySelector('.title');
+  const author = document.querySelector('.author');
+  e.preventDefault();
+  addNewData(title.value, author.value);
+});
+
+function createBook(arr) {
+  let books = '';
+  for (let i = 0; i < arr.length; i++) {
+    books += `
+      <p>${arr[i].title}</p>
+      <p>${arr[i].author}</p>
+      <button onclick = "removeBook(${i})">Remove</button>
+      <hr/>
+    `;
+  }
+  return books;
+}
+
+function displayBooks() {
+  const listOfBooks = document.querySelector('.container');
+  listOfBooks.innerHTML = `
+    <ul>
+      ${createBook(storeData)}
+    </ul>
+  `
+};
+
 function removeBook(i) {
   storeData.splice(i);
   updateData();
@@ -56,6 +55,4 @@ function removeBook(i) {
 }
 removeBook();
 
-window.location.reload = displayBooks()
-
-console.log(window.localStorage.getItem('Added Books'));
+window.location.reload = displayBooks();
